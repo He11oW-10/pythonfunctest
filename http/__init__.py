f0b6efdf3b848874e813cd8632672fe3728a49ce
He11oW-10 <46309857+He11oW-10@users.noreply.github.com>
2021-10-21 18:07:20 +0800

    Update __init__.py

--- http/__init__.py
+++ http/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import numpy
 import scipy
+import time
 import azure.functions as func
 
 
@@ -8,6 +9,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    time.sleep(3600)
     if not name:
         try:
             req_body = req.get_json()
